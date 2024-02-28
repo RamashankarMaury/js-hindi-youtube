bf358dca6eaeb7f1584b0b4917cf0cd006bef43c
RamashankarMaury <161269408+RamashankarMaury@users.noreply.github.com>
2024-02-28 07:55:31 +0000

    container config don

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+ const accountId = 144553
+ let accountEmail = "ramashankr@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+
+ // accountId = 2 // not allowed
+
+ accountEmail = "ramashankar@gmail.com"
+ accountPassword = "13121212"
+ console.log(accountId);
+
+ 
+
+ console.table([accountId, accountEmail, accountPassword, accountCity]);
